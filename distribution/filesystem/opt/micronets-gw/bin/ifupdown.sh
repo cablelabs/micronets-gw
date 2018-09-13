3d408641afc3a61e63241b5cd63f5fb093956171
Craig Pratt <craig@ecaspia.com>
2018-09-12 17:22:52 -0700

    Replaced ifupdown.sh with the version from ovs 2.9.2

--- distribution/filesystem/opt/micronets-gw/bin/ifupdown.sh
+++ distribution/filesystem/opt/micronets-gw/bin/ifupdown.sh
@@ -17,55 +17,28 @@
 # Have a look at /usr/share/doc/openvswitch-switch/README.Debian
 # for more information about configuring the /etc/network/interfaces.
 
-export PATH=/opt/openvswitch/bin:$PATH
-
-debug_log() {
-     logger -t "ovs-ifupdown" -- "$@"
-}
-
 if [ -z "${IF_OVS_TYPE}" ]; then
     exit 0
 fi
 
 ovs_vsctl() {
-    debug_log "running: ovs-vsctl $@"
-    /opt/openvswitch/bin/ovs-vsctl --timeout=5 "$@"
+    ovs-vsctl --timeout=5 "$@"
 }
 
 if (ovs_vsctl --version) > /dev/null 2>&1; then :; else
     exit 0
 fi
 
-SERVICE_UNIT=/usr/lib/systemd/system/openvswitch-nonetwork.service
-if [ -f $SERVICE_UNIT ] && [ -x /usr/bin/systemctl ]; then
-    if ! systemctl --quiet is-active openvswitch-nonetwork.service; then
-        debug_log "starting openvswitch-nonetwork.service"
-        systemctl start openvswitch-nonetwork.service
-    fi
-else
-    if service openvswitch-switch status > /dev/null 2>&1; then
-        debug_log "starting service openvswitch-switch"
-        service openvswitch-switch start
-    fi
+if /etc/init.d/openvswitch-switch status > /dev/null 2>&1; then :; else
+    /etc/init.d/openvswitch-switch start
 fi
 
-debug_log "MODE: ${MODE}"
 if [ "${MODE}" = "start" ]; then
     eval OVS_EXTRA=\"${IF_OVS_EXTRA}\"
 
-    debug_log "IF_OVS_TYPE: ${IF_OVS_TYPE}"
-    debug_log "IFACE: ${IFACE}"
-    debug_log "IF_OVS_EXTRA: ${IF_OVS_EXTRA}"
-    debug_log "IF_OVS_BRIDGE: ${IF_OVS_BRIDGE}"
-    debug_log "IF_OVS_PORTS: ${IF_OVS_PORTS}"
-    debug_log "IF_OVS_MANAGER: ${IF_OVS_MANAGER}"
-    debug_log "IF_OVS_PROTOCOLS: ${IF_OVS_PROTOCOLS}"
-    debug_log "IF_OVS_PORT_REQ: ${IF_OVS_PORT_REQ}"
     case "${IF_OVS_TYPE}" in
         OVSBridge)
                 ovs_vsctl -- --may-exist add-br "${IFACE}" ${IF_OVS_OPTIONS}\
-                         ${IF_OVS_MANAGER+-- set-manager $IF_OVS_MANAGER} \
-                         ${IF_OVS_PROTOCOLS+-- set bridge ${IFACE} protocols=$IF_OVS_PROTOCOLS} \
                          ${OVS_EXTRA+-- $OVS_EXTRA}
 
                 if [ ! -z "${IF_OVS_PORTS}" ]; then
@@ -75,27 +48,26 @@ if [ "${MODE}" = "start" ]; then
         OVSPort)
                 ovs_vsctl -- --may-exist add-port "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_OPTIONS} \
-                    ${IF_OVS_PORT_REQ+-- set Interface ${IFACE} ofport_request=$IF_OVS_PORT_REQ} \
-                    ${OVS_EXTRA+-- $OVS_EXTRA} 
+                    ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ifconfig "${IFACE}" up
+                ip link set "${IFACE}" up
                 ;;
         OVSIntPort)
                 ovs_vsctl -- --may-exist add-port "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_OPTIONS} -- set Interface "${IFACE}"\
                     type=internal ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ifconfig "${IFACE}" up
+                ip link set "${IFACE}" up
                 ;;
         OVSBond)
                 ovs_vsctl -- --fake-iface add-bond "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_BONDS} ${IF_OVS_OPTIONS} \
                     ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ifconfig "${IFACE}" up
+                ip link set "${IFACE}" up
                 for slave in ${IF_OVS_BONDS}
                 do
-                    ifconfig "${slave}" up
+                    ip link set "${IFACE}" up
                 done
                 ;;
         OVSPatchPort)
