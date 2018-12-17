cablelabs/micronets-gw

commit aaf9063889fc876996720f72ffbdc58951aeabbb

Add device endpoint restrictions to the micronets-gw-service (#16)
Add support for filtering device traffic to limit device communication
 to particular hosts via either a "deny" list (a device 'denyHosts'
 property) that will allow connections to hosts other than those listed
 or an "allow" list (a device 'allowHosts' property) that will allow
 connections only to the hosts listed.

Added a new config variable FLOW_ADAPTER_DPCTL_SHOW_FILE
 to allow the interface-to-port list to be provided in a file.
 And removed the hard-coded command to extract it from ovs
 at runtime with config var FLOW_ADAPTER_DPCTL_SHOW_COMMAND.

Added a new config class DnsmasqDevelopmentConfigWithFlowAdapter,
 to allow for testing flow generation without being on an
 OVS-enabled system.

Since we want DNS lookups to be non-blocking, and the doc model
 update needs to be able to write new flows, had to make all
 code paths that update the model async.

Added serialization of device allow/deny lists to the dnsmasq adapter.

Also fixed an issue that was causing truncation of the device ID.
 The device ID is now read from the device "prefix" line instead
 of from the dhcp-host line. (along with the allow/deny lists)

Bumped Debian installer version to 1.0.31

Tested on gateway with 1 and 2 devices, with various `allowHosts'/
 'denyHosts` values. Validated flows and performed basic connection
 tests.

distribution/Makefile
@@ -1,6 +1,6 @@
 .PHONY: all clean distclean deb scp
 
-VERSION=1.0.29
+VERSION=1.0.31
 FBASE=micronets-gw
 SDIRS=../filesystem
 IBASE=/opt

micronets-gw-service/app/open_flow_adapter.py
@@ -1,7 +1,7 @@
-import re, logging, tempfile, subprocess
+import re, logging, tempfile, subprocess, asyncio
 
 from pathlib import Path
-from .utils import blank_line_re, comment_line_re
+from .utils import blank_line_re, comment_line_re, unroll_host_list
 from subprocess import call
 
 logger = logging.getLogger ('micronets-gw-service')
@@ -14,10 +14,17 @@ class OpenFlowAdapter:
     interfaces_ovs_bridge_uplink_re = re.compile ('^\s*ovs_bridge_uplink_port\s+(\w+)\s*$')
     interfaces_ovs_ports_re = re.compile ('\s*ovs_ports\s+([\w ]+)\s*$')
     port_intface_re = re.compile('^\s*port ([0-9]+): (\w+).*$')
+    ovs_dpctl_show_filename = None
+    ovs_dpctl_show_command = None
 
     def __init__ (self, config):
         self.interfaces_file_path = Path (config ['FLOW_ADAPTER_NETWORK_INTERFACES_PATH'])
         self.apply_openflow_command = config['FLOW_ADAPTER_APPLY_FLOWS_COMMAND']
+        if 'FLOW_ADAPTER_DPCTL_SHOW_FILE' in config:
+            # Really just for development/test
+            self.ovs_dpctl_show_filename = config['FLOW_ADAPTER_DPCTL_SHOW_FILE']
+        else:
+            self.ovs_dpctl_show_command = config['FLOW_ADAPTER_DPCTL_SHOW_COMMAND']
 
         with self.interfaces_file_path.open ('r') as infile:
             try:
@@ -31,7 +38,7 @@ class OpenFlowAdapter:
         try:
             self.determine_port_mappings ()
         except Exception as e:
-            raise Exception ("OpenFlowAdapter: Error determining port mppings: {}".format (e))
+            raise Exception ("OpenFlowAdapter: Error determining port mappings: {}".format (e))
 
     def read_interfaces_file (self, infile):
         self.ovs_micronet_interfaces = None
@@ -65,10 +72,18 @@ class OpenFlowAdapter:
         # TODO: Consider using ovsdb-query to get the port mappings
         self.interface_for_port = {}
         self.port_for_interface = {}
-        cp = subprocess.run(["/usr/bin/ovs-dpctl", "show"], stdout=subprocess.PIPE)
-        if not cp or not cp.stdout:
-            raise Exception (f"Error running ovs-dpctl: no stdout")
-        dpctl_out = cp.stdout.decode (encoding="utf-8")
+        if self.ovs_dpctl_show_filename:
+            ovs_dpctl_show_filepath = Path (self.ovs_dpctl_show_filename)
+            with ovs_dpctl_show_filepath.open('r') as infile:
+                try:
+                    dpctl_out = infile.read()
+                except Exception as e:
+                    raise Exception("OpenFlowAdapter: Error reading dpctl-show file: {}".format(e))
+        else:
+            cp = subprocess.run(["/usr/bin/ovs-dpctl", "show"], stdout=subprocess.PIPE)
+            if not cp or not cp.stdout:
+                raise Exception (f"Error running ovs-dpctl: no stdout")
+            dpctl_out = cp.stdout.decode (encoding="utf-8")
 
         logger.info ("Port interfaces:\n"
                      "------------------------------------------------------------------------\n"
@@ -83,29 +98,47 @@ class OpenFlowAdapter:
                 self.interface_for_port[port] = interface
                 self.port_for_interface[interface] = port
 
-    def update (self, subnet_list, device_lists):
+    async def update (self, subnet_list, device_lists):
         logger.info (f"OpenFlowAdapter.update ()")
         logger.info (f"OpenFlowAdapter.update: device_lists: {device_lists}")
+
+        # Note: These flows are a work-in-progress
+
         disabled_interfaces = self.ovs_micronet_interfaces.copy ()
         logger.info (f"OpenFlowAdapter.update: configured interfaces: {disabled_interfaces}")
 
         with tempfile.NamedTemporaryFile (mode='wt') as flow_file:
             flow_file_path = Path (flow_file.name)
-            start_table = 0
-            port_filter_table = 20
             logger.info(f"created temporary file {flow_file_path}")
+            target_bridge = None
+            start_table = 0
+            trunk_table = 2
+            unrestricted_device_table = 3
             flow_file.write ("del\n") # This will clear all flows
-            target_bridge = "brmn001"
+
+            flow_file.write(f"add table={start_table},priority=20,dl_dst=01:80:c2:00:00:00/ff:ff:ff:ff:ff:f0, "
+                            f"actions=drop\n")
+            flow_file.write(f"add table={start_table},priority=20,dl_src=01:00:00:00:00:00/01:00:00:00:00:00, "
+                            f"actions=drop\n")
+            # This NORMAL action for LOCAL means the packet will be delivered
+            flow_file.write(f"add table={start_table},priority=10,in_port=LOCAL "
+                            f"actions=NORMAL\n")
+            flow_file.write(f"add table={start_table},priority=10,in_port=1 "
+                            f"actions=NORMAL\n")
 
             # Walk the subnets
-            cur_subnet_table = 100
+            cur_table = 10
             for subnet_id, subnet in subnet_list.items ():
                 subnet_int = subnet ['interface']
                 subnet_bridge = subnet ['ovsBridge']
-                logger.info (f"Enabling flow for subnet {subnet_id} (interface {subnet_int})")
+                subnet_network = subnet ['ipv4Network']['network']
+                cur_subnet_table = cur_table
+                cur_table += 1
+                logger.info (f"Creating flow table {cur_subnet_table} for subnet {subnet_id} (interface {subnet_int})")
                 if not target_bridge:
                     target_bridge = subnet_bridge
                 else:
+                    # Currently only support all subnets on the same bridge
                     if subnet_bridge != target_bridge:
                         raise Exception(f"subnet {subnet_id} has a different ovsBridge ('{subnet_bridge}')"
                                         f"than other subnets ({target_bridge})")
@@ -117,31 +150,77 @@ class OpenFlowAdapter:
                 subnet_port = self.port_for_interface [subnet_int]
                 flow_file.write (f"add table={start_table},priority=10,in_port={subnet_port} "
                                  f"actions=resubmit(,{cur_subnet_table})\n")
-                # Walk the devices and create a device filter table for each interface
+                # Walk the devices in the interface and create appropriate device filter tables
+                flow_file.write (f"  # TABLE {cur_subnet_table}: micronet {subnet_id}"
+                                 f" (interface {subnet_int}, subnet {subnet_network})\n")
                 for device_id, device in device_lists [subnet_id].items ():
                     device_mac = device ['macAddress']['eui48']
-                    flow_file.write (f"add table={cur_subnet_table},priority=10,dl_src={device_mac} "
-                                     f"actions=resubmit(,{port_filter_table})\n")
-                flow_file.write (f"add table={cur_subnet_table},priority=5 "
+                    host_spec_list = None
+                    if 'allowHosts' in device:
+                        hosts = device['allowHosts']
+                        host_spec_list = await unroll_host_list (hosts)
+                        host_action = "NORMAL"
+                        default_host_action = "drop"
+                        # Add critical hosts to the allow list
+                        host_spec_list.append(subnet['ipv4Network']['gateway'])
+                        if 'nameservers' in subnet:
+                            host_spec_list += subnet['nameservers']
+                        if 'nameservers' in device:
+                            host_spec_list += device['nameservers']
+                    elif 'denyHosts' in device:
+                        hosts = device ['denyHosts']
+                        host_spec_list = await unroll_host_list (hosts)
+                        host_action = "drop"
+                        default_host_action = "NORMAL"
+
+                    if host_spec_list:
+                        cur_dev_table = cur_table
+                        cur_table += 1
+                        flow_file.write (f"  add table={cur_subnet_table},priority=20,dl_src={device_mac} "
+                                         f"actions=resubmit(,{cur_dev_table})\n")
+                        flow_file.write (f"    # TABLE {cur_dev_table}: hosts allowed/denied for device {device_id} (MAC {device_mac})\n")
+                        flow_file.write (f"    add table={cur_dev_table},priority=20,udp,tp_dst=67 "
+                                         f"actions=LOCAL\n")
+                        flow_file.write (f"    add table={cur_dev_table},priority=20,arp "
+                                         f"actions=NORMAL\n")
+                        flow_file.write (f"    #   hosts: {hosts}\n")
+                        for host_spec in host_spec_list:
+                            flow_file.write(f"    add table={cur_dev_table},priority=10,ip,ip_dst={host_spec} "
+                                            f"actions={host_action}\n")
+                        flow_file.write (f"    add table={cur_dev_table},priority=5 "
+                                         f"actions={default_host_action}\n")
+                    else:
+                        flow_file.write (f"  add table={cur_subnet_table},priority=20,dl_src={device_mac} "
+                                         f"actions=resubmit(,{unrestricted_device_table})\n")
+                flow_file.write (f"  add table={cur_subnet_table},priority=5 "
                                  f"actions=drop\n")
-                cur_subnet_table += 1
             for interface in disabled_interfaces:
                 logger.info (f"Disabling flow for interface {interface}")
                 subnet_port = self.port_for_interface [interface]
                 flow_file.write (f"add table={start_table},priority=10,in_port={subnet_port} "
                                  f"actions=drop\n")
-            # All requests that aren't associated with a micronet go to NORMAL
-            flow_file.write (f"add table={start_table},priority=5 "
-                             f"actions=NORMAL\n")
             # The common port filtering rules (after a packet has flowed through the
             #  interface and mac tables
 
-            # Don't allow DHCP requests to flow out of the gateway
-            flow_file.write (f"add table={port_filter_table},priority=10,udp,tp_dst=67 "
+            # Table for all trunk traffic
+            flow_file.write (f"add table={start_table},priority=10,in_port={self.ovs_uplink_interface} "
+                             f"actions=resubmit(,{trunk_table})\n")
+            flow_file.write(f"  # TABLE {trunk_table}: Trunk ingress table\n")
+            flow_file.write (f"  add table={trunk_table},priority=10,udp,tp_dst=67 "
                              f"actions=LOCAL\n")
-            # TODO: Consider blocking other traffic (all broadcast packets?)
-            flow_file.write (f"add table={port_filter_table},priority=5 "
+            flow_file.write (f"  add table={trunk_table},priority=5 "
                              f"actions=NORMAL\n")
+
+            # All requests that don't match a known port are dropped (like a cold stone)
+            flow_file.write (f"add table={start_table},priority=5 "
+                             f"actions=drop\n")
+
+            # Table for all devices with no restrictions (no allowHosts/denyHosts)
+            flow_file.write(f"  # TABLE {unrestricted_device_table}: Unrestricted device table (no allowHosts/denyHosts)\n")
+            flow_file.write (f"  add table={unrestricted_device_table},priority=10,udp,tp_dst=67 "
+                             f"actions=LOCAL\n")
+            flow_file.write (f"  add table={unrestricted_device_table},priority=5 "
+                             f"actions=drop\n")
             flow_file.flush ()
 
             with flow_file_path.open('r') as infile:
@@ -149,7 +228,7 @@ class OpenFlowAdapter:
                 logger.info ("Issuing new flows:")
                 logger.info ("------------------------------------------------------------------------")
                 for line in infile:
-                    logger.info (line)
+                    logger.info (line[0:-1])
                 logger.info ("------------------------------------------------------------------------")
 
             run_cmd = self.apply_openflow_command.format (target_bridge, flow_file_path)

micronets-gw-service/app/__init__.py
@@ -108,7 +108,7 @@ try:
     else:
         logger.info("Not starting OpenFlowAdapter (adapter disabled in config)")
 except Exception as ex:
-    logger.warning ("Error staring flow adapter:", exc_info=True)
+    logger.warning ("Error starting flow adapter:", exc_info=True)
 
 try:
     min_dhcp_conf_update_int_s = app.config ['MIN_DHCP_UPDATE_INTERVAL_S']
@@ -117,6 +117,9 @@ try:
 except Exception as ex:
     logger.info ("Error starting with adapter:", exc_info=True)
     exit (1)
+
+if flow_adapter:
+    asyncio.ensure_future(dhcp_conf_model.update_conf())
 
 # Initialize the API
 from . import dhcp_api

micronets-gw-service/app/dhcp_api.py
@@ -1,7 +1,7 @@
 from quart import request, jsonify
 from ipaddress import IPv4Address, IPv4Network
 from app import app, get_dhcp_conf_model
-from .utils import InvalidUsage
+from .utils import InvalidUsage, get_ipv4_addrs_for_hostspec
 
 import re
 import netaddr
@@ -108,7 +108,7 @@ def check_ipv4_address_field (json_obj, ip_addr_field, required):
     return ip_address
 
 def check_nameservers (container, field_name, required):
-    nameservers = check_field (container, 'nameservers', (list), False)
+    nameservers = check_field (container, field_name, (list), required)
     if nameservers:
         for ip_address in nameservers:
             ipv4_address_error = get_ipv4_address_error (ip_address)
@@ -130,7 +130,7 @@ def check_subnet (subnet, subnet_id=None, required=True):
     subnet_id = subnet_id.lower ()
     check_subnet_id (subnet_id, subnet)
     check_ipv4_network (subnet, subnet_id, required)
-    check_nameservers (subnet, 'nameservers', required)
+    check_nameservers (subnet, 'nameservers', False)
     check_field (subnet, 'ovsBridge', str, required)
     check_field (subnet, 'interface', str, required)
 
@@ -146,19 +146,19 @@ async def create_subnets ():
     if 'subnets' in top_level:
         subnets = top_level ['subnets']
         check_subnets (subnets, required=True)
-        return get_dhcp_conf_model ().create_subnets (subnets)
+        return await get_dhcp_conf_model ().create_subnets (subnets)
     elif 'subnet' in top_level:
         subnet = top_level ['subnet']
         check_subnet (subnet, required=True)
-        return get_dhcp_conf_model ().create_subnet (subnet)
+        return await get_dhcp_conf_model ().create_subnet (subnet)
 
 @app.route (dhcp_api_prefix + '/subnets', methods=['GET'])
 async def get_all_subnets ():
-    return get_dhcp_conf_model ().get_all_subnets ()
+    return await get_dhcp_conf_model ().get_all_subnets ()
 
 @app.route (dhcp_api_prefix + '/subnets', methods=['DELETE'])
 async def delete_all_subnets ():
-    return get_dhcp_conf_model ().delete_all_subnets ()
+    return await get_dhcp_conf_model ().delete_all_subnets ()
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>', methods=['PUT'])
 async def update_subnet (subnet_id):
@@ -170,20 +170,20 @@ async def update_subnet (subnet_id):
     check_for_unrecognized_entries (top_level, ['subnet'])
     subnet = top_level ['subnet']
     check_subnet (subnet, subnet_id=subnet_id, required=False)
-    updated_subnet = get_dhcp_conf_model ().update_subnet (subnet, subnet_id)
+    updated_subnet = await get_dhcp_conf_model ().update_subnet (subnet, subnet_id)
     return updated_subnet
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>', methods=['GET'])
 async def get_subnet (subnet_id):
     subnet_id = subnet_id.lower ()
     check_subnet_id (subnet_id, request.path)
-    return get_dhcp_conf_model ().get_subnet (subnet_id)
+    return await get_dhcp_conf_model ().get_subnet (subnet_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>', methods=['DELETE'])
 async def delete_subnet (subnet_id):
     subnet_id = subnet_id.lower()
     check_subnet_id (subnet_id, request.path)
-    return get_dhcp_conf_model ().delete_subnet (subnet_id)
+    return await get_dhcp_conf_model ().delete_subnet (subnet_id)
 
 device_id_re = re.compile ('^\w+[-.\w]*$', re.ASCII)
 
@@ -200,8 +200,19 @@ def check_mac_address_field (json_obj, mac_addr_field, required):
         raise InvalidUsage (400, message=f"Supplied MAC '{mac_field}' in '{mac_addr_field}' is not valid")
     return mac_field
 
-def check_device (device, required):
-    check_for_unrecognized_entries (device, ['deviceId','macAddress','networkAddress'])
+async def check_hostspecs (container, field_name, required):
+    hosts = check_field (container, field_name, (list), required)
+    if hosts:
+        for host in hosts:
+            try:
+                hosts = await get_ipv4_addrs_for_hostspec (host)
+            except Exception as ex:
+                raise InvalidUsage (400, message=f"Supplied hostname '{host}' in field '{field_name}' "
+                                    f"of '{container}' is not valid: {ex}")
+    return hosts
+
+async def check_device (device, required):
+    check_for_unrecognized_entries (device, ['deviceId', 'macAddress', 'networkAddress', 'allowHosts', 'denyHosts'])
     device_id = check_field (device, 'deviceId', str, required)
     if device_id:
         device_id = device_id.lower ()
@@ -223,9 +234,12 @@ def check_device (device, required):
         check_for_unrecognized_entries (network_address, ['ipv4'])
         check_ipv4_address_field (network_address, 'ipv4', required)
 
-def check_devices (devices, required):
+    await check_hostspecs (device, 'allowHosts', False)
+    await check_hostspecs (device, 'denyHosts', False)
+
+async def check_devices (devices, required):
     for device in devices:
-        check_device (device, required)
+        await check_device (device, required)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices', methods=['POST'])
 async def create_devices (subnet_id):
@@ -235,24 +249,24 @@ async def create_devices (subnet_id):
     check_for_unrecognized_entries (top_level, ['device', 'devices'])
     if 'devices' in top_level:
         devices = top_level ['devices']
-        check_devices (devices, required=True)
-        return get_dhcp_conf_model ().create_devices (devices, subnet_id)
+        await check_devices (devices, required=True)
+        return await get_dhcp_conf_model ().create_devices (devices, subnet_id)
     elif 'device' in top_level:
         device = top_level ['device']
-        check_device (device, required=True)
-        return get_dhcp_conf_model ().create_device (device, subnet_id)
+        await check_device (device, required=True)
+        return await get_dhcp_conf_model ().create_device (device, subnet_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices', methods=['GET'])
 async def get_devices (subnet_id):
     subnet_id = subnet_id.lower ()
     check_subnet_id (subnet_id, request.path)
-    return get_dhcp_conf_model ().get_all_devices (subnet_id)
+    return await get_dhcp_conf_model ().get_all_devices (subnet_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices', methods=['DELETE'])
 async def delete_devices (subnet_id):
     subnet_id = subnet_id.lower()
     check_subnet_id (subnet_id, request.path)
-    return get_dhcp_conf_model ().delete_all_devices (subnet_id)
+    return await get_dhcp_conf_model ().delete_all_devices (subnet_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices/<device_id>', methods=['PUT'])
 async def update_device (subnet_id, device_id):
@@ -264,8 +278,8 @@ async def update_device (subnet_id, device_id):
     top_level = await request.get_json ()
     check_for_unrecognized_entries (top_level, ['device'])
     device_update = top_level ['device']
-    check_device (device_update, required=False)
-    return get_dhcp_conf_model ().update_device (device_update, subnet_id, device_id)
+    await check_device (device_update, required=False)
+    return await get_dhcp_conf_model ().update_device (device_update, subnet_id, device_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices/<device_id>', methods=['GET'])
 async def get_device (subnet_id, device_id):
@@ -273,7 +287,7 @@ async def get_device (subnet_id, device_id):
     device_id = device_id.lower ()
     check_subnet_id (subnet_id, request.path)
     check_device_id (device_id, request.path)
-    return get_dhcp_conf_model ().get_device (subnet_id, device_id)
+    return await get_dhcp_conf_model ().get_device (subnet_id, device_id)
 
 @app.route (dhcp_api_prefix + '/subnets/<subnet_id>/devices/<device_id>', methods=['DELETE'])
 async def delete_device (subnet_id, device_id):
@@ -281,7 +295,7 @@ async def delete_device (subnet_id, device_id):
     device_id = device_id.lower ()
     check_subnet_id (subnet_id, request.path)
     check_device_id (device_id, request.path)
-    return get_dhcp_conf_model ().delete_device (subnet_id, device_id)
+    return await get_dhcp_conf_model ().delete_device (subnet_id, device_id)
 
 async def check_lease_event (lease_event):
     event_fields = check_field (lease_event, 'leaseChangeEvent', dict, True)

commit 9b9241a1b6e8eacf105d7a2e5a844d26b6da8f96

Change the default action for the unrestricted device table to accept (issue 23)
This issue was introduced in PR #16.

Issue was seen during demo testing. Tested this as a hotfix.

distribution/Makefile
@@ -1,6 +1,6 @@
 .PHONY: all clean distclean deb scp
 
-VERSION=1.0.33
+VERSION=1.0.34
 FBASE=micronets-gw
 SDIRS=../filesystem
 IBASE=/opt

micronets-gw-service/app/open_flow_adapter.py
@@ -215,7 +215,7 @@ class OpenFlowAdapter:
             flow_file.write (f"  add table={unrestricted_device_table},priority=10,udp,tp_dst=67 "
                              f"actions=LOCAL\n")
             flow_file.write (f"  add table={unrestricted_device_table},priority=5 "
-                             f"actions=drop\n")
+                             f"actions=NORMAL\n")
             flow_file.flush ()
 
             with flow_file_path.open('r') as infile: