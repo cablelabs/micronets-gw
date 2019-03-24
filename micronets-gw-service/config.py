97b0794f921c22bc50f036b9500a32b92515115f
Craig Pratt <craig@ecaspia.com>
2019-03-23 17:18:28 -0700

    WIP Rewrite of flow generation logic based on newest prototype
    Still missing the logic for processing of Device
     toRules and fromRules.
    
    Tested via comparison of generated flows with prototype
     flows.

--- micronets-gw-service/config.py
+++ micronets-gw-service/config.py
@@ -22,7 +22,7 @@ class BaseConfig:
     WEBSOCKET_TLS_CA_CERT_FILE = pathlib.Path (__file__).parent.joinpath ('lib/micronets-ws-root.cert.pem')
     FLOW_ADAPTER_NETWORK_INTERFACES_PATH = "/etc/network/interfaces"
     # For this command, the first parameter will be the bridge name and the second the flow filename
-    FLOW_ADAPTER_APPLY_FLOWS_COMMAND = '/usr/bin/ovs-ofctl add-flows {} {}'
+    FLOW_ADAPTER_APPLY_FLOWS_COMMAND = '/usr/bin/ovs-ofctl add-flows {ovs_bridge} {flow_file}'
     FLOW_ADAPTER_ENABLED = False
 
 #
@@ -94,7 +94,7 @@ class DnsmasqDevelopmentConfigWithWebsocket (DnsmasqDevelopmentConfig):
     WEBSOCKET_SERVER_ADDRESS = "localhost"
 
 class DnsmasqDevelopmentConfigWithFlowAdapter (DnsmasqDevelopmentConfig):
-    FLOW_ADAPTER_APPLY_FLOWS_COMMAND = '/bin/echo This is where I would add flows to bridge {} from {}'
+    FLOW_ADAPTER_APPLY_FLOWS_COMMAND = '/usr/bin/sort -t= -k 2n -k 3rn {flow_file}'
     FLOW_ADAPTER_ENABLED = True
 
 class DnsmasqTestingConfig (BaseDnsmasqConfig):
