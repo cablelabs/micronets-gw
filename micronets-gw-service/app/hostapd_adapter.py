2486de245cf155748225adad50e0c0d28839e8b3
Craig Pratt <craig@ecaspia.com>
2019-04-07 18:18:57 -0700

    WIP Basic communication prototype

--- micronets-gw-service/app/hostapd_adapter.py
+++ micronets-gw-service/app/hostapd_adapter.py
@@ -6,7 +6,10 @@ import inspect
 import time
 import fcntl
 import os
+import subprocess
+import threading
 from asyncio import BaseTransport, ReadTransport, WriteTransport, SubprocessTransport
+from subprocess import Popen, PIPE
 
 logger = logging.getLogger ('hostapd_adapter')
 
@@ -15,104 +18,36 @@ class HostapdAdapter:
     def __init__ (self, hostapd_cli_path, hostapd_cli_args=()):
         self.hostapd_cli_path = hostapd_cli_path
         self.hostapd_cli_args = hostapd_cli_args
-        self.hostapd_proc = None
-        self.transport = None
-        self.protocol = None
+        self.hostapd_cli_process = None
+        self.process_reader_thread = None
 
 
     async def connect(self):
         logger.info(f"HostapdAdapter:connect()")
         event_loop = asyncio.get_event_loop ()
         logger.info(f"HostapdAdapter:connect: Running {self.hostapd_cli_path} {self.hostapd_cli_args}")
-#        code = "import time,datetime; print(datetime.datetime.now());time.sleep(4);print(datetime.datetime.now());time.sleep(4);print(datetime.datetime.now());"
-#        code = 'import time,datetime; \nwhile True: \n\tprint(datetime.datetime.now())\n\ttime.sleep(1)\n'
-        transport, protocol = await event_loop.subprocess_exec(HostapdConnection, 
-                                                               self.hostapd_cli_path, *self.hostapd_cli_args,
-                                                               stdout=asyncio.subprocess.PIPE, 
-                                                               stderr=asyncio.subprocess.STDOUT, 
-                                                               stdin=asyncio.subprocess.PIPE,
-                                                               encoding="utf-8")
-        logger.info(f"HostapdAdapter:connect: subprocess_exec returned: transport {transport}, protocol {protocol}")
-        self.transport = transport
-        self.protocol = protocol
-        protocol.stdin.writelines([b"help()\n   "])
-        # protocol.stdin_pipe.flush()
-        # protocol.stdout_pipe.flush()
-        # time.sleep(1)
-        # protocol.stdin.writelines([b"topics\n"])
-        # time.sleep(1)
-        # protocol.stdin.writelines([b"modules\n"])
-        # time.sleep(1)
-        # protocol.stdin.write_eof()
-        logger.info(f"HostapdAdapter:connect done")
+        
+        self.hostapd_cli_process = Popen([self.hostapd_cli_path, *self.hostapd_cli_args],
+                                         shell=False, bufsize=1,
+                                         stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
 
-#async def relay_messages(self):
-#        logger.info(f"HostapdAdapter:relay_messages()")
-#        done, pending = await asyncio.wait ([self.read_stdout(), self.read_stderr(), self.write_stdin()],
-#                                            return_when=asyncio.FIRST_COMPLETED)
-#        logger.info(f"HostapdAdapter:relay_messages done")
+        self.process_reader_thread = threading.Thread(target=self.read_process_data)
+        self.process_reader_thread.start()
 
 
-class HostapdConnection (asyncio.Protocol):
-    def __init__(self):
-        logger.info(f"HostapdConnection constructed")
-        self.transport = None
-        self.stdin = None
-        self.stdin_pipe = None
-        self.stdout = None
+    def read_process_data(self):
+        while True:
+            try:
+                data = self.hostapd_cli_process.stdout.readline().decode("utf-8")
+                if not data:
+                    break
+                logger.info(f"HostapdAdapter:read_process_data: Read data: {data.rstrip()}")
+            except Exception as ex:
+                logger.warning(f"HostapdAdapter:read_process_data: Error processing data: {ex}")
 
-    def connection_made(self, transport):
-        logger.info(f"HostapdConnection:connection_made(transport: {transport})")
-        if transport:
-            self.transport = transport
-            self.protocol = transport.get_protocol()
-            logger.info(f"HostapdConnection:connection_made:   protocol: {self.protocol}")
-            logger.info(f"HostapdConnection:connection_made:   transport type: {type(transport)}")
-            
-            if isinstance(transport, asyncio.ReadTransport):
-                logger.info(f"HostapdConnection:connection_made:   transport is a ReadTransport")
-            if isinstance(transport, asyncio.WriteTransport):
-                logger.info(f"HostapdConnection:connection_made:   transport is a WriteTransport")
-            if isinstance(transport, asyncio.SubprocessTransport):
-                logger.info(f"HostapdConnection:connection_made:   transport is a SubprocessTransport")
-                self.stdin = transport.get_pipe_transport(0)
-                logger.info(f"HostapdConnection:connection_made:     stdin: {self.stdin}")
-                logger.info(f"HostapdConnection:connection_made:     stdin: write_buffer_limit: {self.stdin.get_write_buffer_limits()}")
-                # self.stdin.set_write_buffer_limits(10, 1)
-                logger.info(f"HostapdConnection:connection_made:     stdin: adjusted write_buffer_limit: {self.stdin.get_write_buffer_limits()}")
-                logger.info(f"HostapdConnection:connection_made:     stdin: write_buffer_size: {self.stdin.get_write_buffer_size()}")
-                self.stdin_pipe = self.stdin.get_extra_info("pipe", None)
-                logger.info(f"HostapdConnection:connection_made:     stdin: pipe: {self.stdin_pipe}")
-                logger.info(f"HostapdConnection:connection_made:     stdin: fileno: {self.stdin_pipe.fileno()}")
-                # logger.info(f"HostapdConnection:connection_made:     stdin members: {inspect.getmembers (self.stdin_pipe)}")
-                # make stdin non-blocking 
-                fl = fcntl.fcntl(self.stdin_pipe, fcntl.F_GETFL)
-                fcntl.fcntl(self.stdin_pipe, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-                self.stdout = transport.get_pipe_transport(1)
-                logger.info(f"HostapdConnection:connection_made:     stdout: {self.stdout}")
-                self.stdout_pipe = self.stdout.get_extra_info("pipe", None)
-                logger.info(f"HostapdConnection:connection_made:     stdout: pipe: {self.stdout_pipe}")
-                logger.info(f"HostapdConnection:connection_made:     stdout: resuming reading")
-
-                fd = self.stdout_pipe.fileno()
-                fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-                fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-                # logger.info(f"HostapdConnection:connection_made:     stdin members: {inspect.getmembers (self.stdin)}")
-
-    def pipe_data_received(self, fd, data):
-        logger.info(f"HostapdConnection:pipe_data_received(fd: {fd}, data: {data})")
-    
-    def pipe_connection_lost(self, fd, exc):
-        logger.info(f"HostapdConnection:pipe_connection_lost(fd: {fd}, exc: {exc})")
-    
-    def process_exited(self):
-        logger.info(f"HostapdConnection:process_exited()")
-    
-    def error_received(self, exc):
-        logger.info(f"HostapdConnection:error_received(exc: {exc})")
-
-    def connection_lost(self, exc):
-        logger.info(f"HostapdConnection:connection_lost(exc: {exc})")
+    def send_command(self, message):
+        self.hostapd_cli_process.stdin.write((message + '\n').encode())	
+        self.hostapd_cli_process.stdin.flush()
 
 if __name__ == '__main__':
     print (f"{__name__}: Starting\n")
@@ -123,15 +58,17 @@ if __name__ == '__main__':
 
     event_loop = asyncio.get_event_loop ()
     try:
-        logger.info (f"Starting event loop...")
+        logger.info (f"{__name__}: Starting event loop...")
 #        hostapd_adapter = HostapdAdapter("/usr/bin/tail", ["-f", "-n", "1", "/var/log/syslog"])
-        hostapd_adapter = HostapdAdapter("/usr/bin/python", ["-u"])
+        hostapd_adapter = HostapdAdapter("/opt/micronets-hostapd/bin/hostapd_cli", [])
         event_loop.run_until_complete(hostapd_adapter.connect())
-        event_loop.run_forever()
+        logger.info (f"{__name__}: Connected.")
+        time.sleep(2)
+        logger.info (f"{__name__}: Issuing help command...")
+        hostapd_adapter.send_command("help")
 #        event_loop.run_until_complete(hostapd_adapter.relay_messages())
         event_loop.run_forever()
     except Exception as Ex:
         logger.warn (f"Caught an exception: {Ex}")
-        traceback.print_exc (file=sys.stdout)
-    
+
 
