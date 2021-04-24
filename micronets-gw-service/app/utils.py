6527f1ec673b2b2937beb23af921631fdc73e548
Craig Pratt <c.pratt@cablelabs.com>
2021-04-24 00:41:52 -0700

    Fixed a small bug in utils.merge_deep()

--- micronets-gw-service/app/utils.py
+++ micronets-gw-service/app/utils.py
@@ -40,7 +40,7 @@ def merge_deep (dict1, dict2):
     result = deepcopy (dict1)
     for key, value in dict2.items ():
         if isinstance (value, collections.Mapping):
-            result [key] = merge (result.get (key, {}), value)
+            result [key] = merge_deep (result.get (key, {}), value)
         else:
             result [key] = deepcopy (dict2 [key])
     return result
