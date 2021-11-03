590f091e0425a2142437b76405395313d00b44da
Craig Pratt <c.pratt@cablelabs.com>
2021-11-02 18:53:29 -0700

    Implemented controller and broker retry logic (NETREACH_ADAPTER_CONN_RETRY_S) (#48)
    Addresses Issue #53

--- micronets-gw-service/config.py
+++ micronets-gw-service/config.py
@@ -54,6 +54,7 @@ class NetreachDefaultSettings():
     # NETREACH_ADAPTER_MQTT_BROKER_URL = "mqtts://staging.broker.controller.netreach.in:8885" # for overriding
     NETREACH_ADAPTER_MQTT_CA_CERTS = libpath.joinpath('netreach-mqtt-ca.crt')
     NETREACH_ADAPTER_CONN_START_DELAY_S = 2
+    NETREACH_ADAPTER_CONN_RETRY_S = 10
     NETREACH_ADAPTER_USE_DEVICE_PASS = True
 
 #
