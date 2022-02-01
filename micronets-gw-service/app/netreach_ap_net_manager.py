b69ecbe420e5164fc344ed02869c06b22b35ff81
Craig Pratt <c.pratt@cablelabs.com>
2022-02-01 03:40:35 -0800

    Added copyright header

--- micronets-gw-service/app/netreach_ap_net_manager.py
+++ micronets-gw-service/app/netreach_ap_net_manager.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2022 Cable Television Laboratories, Inc. ("CableLabs")
+#                    and others.  All rights reserved.
+#
+# Licensed in accordance of the accompanied LICENSE.txt or LICENSE.md
+# file in the base directory for this project. If none is supplied contact
+# CableLabs for licensing terms of this software.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging, json, re, asyncio, httpx, hashlib, subprocess, tempfile
 from pathlib import Path
 from uuid import UUID
